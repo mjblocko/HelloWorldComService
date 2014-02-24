b9fbee21d6ad4f440fadb20524d844e83393c973
Block <blockm@mspredhatsrv2.firepond.com>
2014-02-24 07:30:10 -0600

    changes by mingq

---
+++ HelloWorldComService/HelloWorldComService/doWidl.sh
@@ -0,0 +1,5 @@
+/home/blockm/ics/trunk/trunk/compilers/cross64/build/widl/widl \
+-I ./idl \
+-I /opt/mingw32-w64/x86_64-w64-mingw32/include \
+-t  -h -u -o HelloWorldComService.tlb   HelloWorldComService.idl
+
---
+++ HelloWorldComService/HelloWorldComService/doWindRes.sh
@@ -0,0 +1,3 @@
+x86_64-w64-mingw32-windres  \
+   -i HelloWorldComService.rc \
+   -o build/obj/windows/HelloWorldComService.o \
